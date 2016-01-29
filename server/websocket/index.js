3a104db424e4415f7a94fbd07bfbcacaadcf764a
krishansolanki <krishan.solanki@grapeshot.co.uk>
2016-01-29 15:05:33 +0000

    added socket client connection to index html

--- server/websocket/index.js
+++ server/websocket/index.js
@@ -3,11 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('someone connected');
+  socket.on('getShips', function(){
+
+  });
 });
 
 http.listen(3000, function(){
