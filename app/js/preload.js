8cc2735d53db464f2e4b4119df3f5625cab1808d
davidbuttar <david.buttar@gmail.com>
2016-01-29 16:00:22 +0000

    Falcon addition

--- app/js/preload.js
+++ app/js/preload.js
@@ -3,9 +3,9 @@ var Preload = function(game){};
 Preload.prototype = {
 
 	preload: function(){
-        game.load.image('space', 'assets/deep-space.jpg');
+        game.load.image('space', 'assets/background.jpg');
         game.load.image('bullet', 'assets/bullets.png');
-        game.load.image('ship', 'assets/ship.png');
+        game.load.image('ship', 'assets/ship2.png');
 	},
 
 	create: function(){
---
+++ app/js/shipManager.js
@@ -0,0 +1,40 @@
+var shipManager = function(){
+    var that = {};
+    var ships = [];
+    var active = 0;
+    /**
+     * Add ship to game.
+     *
+     * @param game
+     * @param posX
+     * @param posY
+     */
+    that.add = function(game, posX, posY){
+        //  Our player ship
+        var sprite = game.add.sprite(posX, posY, 'ship');
+        sprite.scale.setTo(0.22, 0.22);
+        sprite.anchor.set(0.403,0.467);
+
+        //  and its physics settings
+        game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+        sprite.body.drag.set(100);
+        sprite.body.maxVelocity.set(200);
+        ships.push(sprite);
+    };
+
+    that.active = function(){
+        return ships[active];
+    };
+
+    /**
+     * Get a particular ship by index
+     * @param index
+     * @returns {*}
+     */
+    that.getShip = function(index){
+        return ships[index];
+    };
+
+    return that;
+};
