d118eb34e20584305675aae5f8ae985eb57fb32f
rafspiny <rafspiny@gmail.com>
2016-01-29 16:49:29 +0000

    Adding a workig versio of socket.io

---
+++ server/Universe.js
@@ -0,0 +1,16 @@
+function Universe() {
+	this.listOfShips = [];
+	this.listOfBullets = [];
+}
+
+/*
+ * Update the universe...woow!
+ */
+Universe.prototype.update = function() {
+	// Take the positios of every ships
+	// Go trough the list of objects and check for collision, skipping dead ship
+
+	// Update the position and the status of ever ship
+}
+
+module.exports = Universe;
--- server/websocket/index.js
+++ server/websocket/index.js
@@ -1,19 +1,40 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Universe = require('../Universe.js');
 
-app.get('/', function(req, res){
+// Our Universe
+var game = new Universe();
 
+// List of connected players
+var listOfPlayerName = [];
+
+// Let's serve an exmaple page to trigger some test events from the clients
+app.get('/websocket/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  
+  // add the ship to the Universe
   console.log('someone connected');
-  socket.on('getShips', function(){
-
+  // var playerName = game.addPlayer();
+  
+  socket.on('disconnect', function(playerName){
+    console.log('user '+playerName +' disconnected');
+    // TODO Remove the user and all of its associated bullets from the Universe
+    // game.removePlayer(playerName);
   });
+
+  socket.on('position_update', function(msg){
+    console.log('message received from ' + msg.player);
+    console.log('message: ' + msg);
+  });
+  
 });
 
 http.listen(3000, function(){
+  console.log('Hello WebSocket');
   console.log('listening on *:3000');
 });
 
